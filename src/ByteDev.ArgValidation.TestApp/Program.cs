e7f4632f76ed403dccffdd31c891bb2800d51536
Paul Stanley <paulstanley@protonmail.com>
2020-02-05 17:48:56 +0800

    Added TestApp for nuget package

---
+++ src/ByteDev.ArgValidation.TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ByteDev.ArgValidation.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MustBeGreaterThan(1);
+        }
+
+        private static void MustBeGreaterThan(int count)
+        {
+            ArgValidator.GreaterThan(count, 2, nameof(count));
+        }
+    }
+}
